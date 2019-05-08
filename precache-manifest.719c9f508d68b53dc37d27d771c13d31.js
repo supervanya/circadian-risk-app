b7d57e94c94c8ca9ac7fcc56ac8cf2cedb45a3e0
supervanya <vano@umich.edu>
2019-05-08 19:58:18 -0400

    Updates

---
+++ precache-manifest.719c9f508d68b53dc37d27d771c13d31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af8ff58bf1d8e71cd185a9eaf46f3cf",
+    "url": "/circadian-risk-app/index.html"
+  },
+  {
+    "revision": "23c05e2896eb40e94316",
+    "url": "/circadian-risk-app/static/css/main.b4aa49b9.chunk.css"
+  },
+  {
+    "revision": "01d5a8e197505915d808",
+    "url": "/circadian-risk-app/static/js/2.ede10acd.chunk.js"
+  },
+  {
+    "revision": "23c05e2896eb40e94316",
+    "url": "/circadian-risk-app/static/js/main.2a7067f9.chunk.js"
+  },
+  {
+    "revision": "0d6d9cf283bad7c58066",
+    "url": "/circadian-risk-app/static/js/runtime~main.b43152e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/circadian-risk-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
