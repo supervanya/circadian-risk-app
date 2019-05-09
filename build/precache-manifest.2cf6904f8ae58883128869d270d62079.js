274a9c191083ee7b413498b0c7807f93d820066b
Vanya Prokopovich <vano@umich.edu>
2019-05-08 20:40:26 -0400

    Add files via upload

---
+++ build/precache-manifest.2cf6904f8ae58883128869d270d62079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9375b510ead4f1ecae4753c8bcb79f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fd714a6505ae1af1c08",
+    "url": "/static/css/main.b4aa49b9.chunk.css"
+  },
+  {
+    "revision": "01d5a8e197505915d808",
+    "url": "/static/js/2.ede10acd.chunk.js"
+  },
+  {
+    "revision": "0fd714a6505ae1af1c08",
+    "url": "/static/js/main.5226ef48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
